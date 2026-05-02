b76eba99b444c2cc022b70371850fb049ff0210c
Dmitrii Koriakov <dm.koryakov@gmail.com>
2026-05-03 01:27:33 +0200

    added test gpu

---
+++ jetson-nano-ability-instruction-based-on-b5050/run/test-gpu.sh
@@ -0,0 +1,2 @@
+./llama-cli -hf unsloth/gemma-3-1b-it-GGUF:UD-IQ1_M --n-gpu-layers 99 -p "Explain quantum entanglement" -n 200 --no-conversation
+./llama-cli -hf unsloth/gemma-3-1b-it-GGUF:UD-IQ1_M --n-gpu-layers 0 -p "Explain quantum entanglement" -n 200 --no-conversation
