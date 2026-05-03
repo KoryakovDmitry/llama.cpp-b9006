47d3bb6342587985ff208b8720a489f9244194fb
Dmitrii Koriakov <dm.koryakov@gmail.com>
2026-05-03 03:09:55 +0200

    UD-Q4_K_XL

--- jetson-nano-ability-instruction-based-on-b5050/run/start-test.sh
+++ jetson-nano-ability-instruction-based-on-b5050/run/start-test.sh
@@ -1,2 +1,2 @@
 llama-cli -hf unsloth/gemma-3-4b-it-GGUF:Q2_K --n-gpu-layers 99
-llama-cli -hf unsloth/Qwen3.5-0.8B-GGUF:UD-Q2_K_XL --n-gpu-layers 99 --reasoning-budget 128
+llama-cli -hf unsloth/Qwen3.5-0.8B-GGUF:UD-Q4_K_XL--n-gpu-layers 99 --reasoning-budget 128
