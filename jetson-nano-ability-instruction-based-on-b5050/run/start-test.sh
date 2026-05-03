b2a68066c9b486c6d8d034ce052d05ee2d95acd6
Dmitrii Koriakov <dm.koryakov@gmail.com>
2026-05-03 03:25:15 +0200

    rllama-cli -hf unsloth/Qwen3.5-0.8B-GGUF:Q8_0 --n-gpu-layers 99

--- jetson-nano-ability-instruction-based-on-b5050/run/start-test.sh
+++ jetson-nano-ability-instruction-based-on-b5050/run/start-test.sh
@@ -1,4 +1,5 @@
 rllama-cli -hf unsloth/gemma-3-4b-it-GGUF:Q2_K --n-gpu-layers 99
-rllama-cli -hf unsloth/Qwen3.5-0.8B-GGUF:UD-Q4_K_XL--n-gpu-layers 99 --reasoning-budget 128
+rllama-cli -hf unsloth/Qwen3.5-0.8B-GGUF:UD-Q4_K_XL --n-gpu-layers 99 --reasoning-budget 128
 rllama-cli -hf unsloth/Qwen3.5-2B-GGUF:UD-IQ2_XXS --n-gpu-layers 99 --reasoning-budget 64
 rllama-cli -hf unsloth/Qwen3.5-2B-GGUF:UD-IQ2_M --n-gpu-layers 99 --reasoning-budget 0
+rllama-cli -hf unsloth/Qwen3.5-0.8B-GGUF:Q8_0 --n-gpu-layers 99
